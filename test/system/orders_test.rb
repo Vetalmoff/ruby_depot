f421877812d70916504d3b0a6e76dbd491510317
Vitalii Tymofiichuk <vetalmoff@gmail.com>
2020-03-06 21:24:32 +0200

    React with 1 test

--- test/system/orders_test.rb
+++ test/system/orders_test.rb
@@ -1,9 +1,37 @@
 require "application_system_test_case"
 
 class OrdersTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit orders_url
+  test "check routing number" do
+    visit store_index_url
+
+    first('.catalog li').click_on 'Add to Cart'
+
+    click_on 'Checkout'
+
+    fill_in 'order_name', with: 'Dave Thomas'
+    fill_in 'order_address', with: '123 Main Street'
+    fill_in 'order_email', with: 'dave@example.com'
+
+    assert_no_selector "#order_routing_number"
+
+    select 'Check', from: 'pay_type'
+    assert_selector "#order_routing_number"
+    assert_selector "#order_account_number"
+
+    select 'Credit card', from: 'pay_type'
+    assert_selector '#order_credit_card_number'
+    assert_selector '#order_experation_date'
+
+    select 'Purchase order', from: 'pay_type'
+    assert_selector '#order_po_number'
+
+  end
+
+  # test "Add to Cart" do
+  #   visit store_index_url
   #
-  #   assert_selector "h1", text: "Order"
-  # end
-end
+  #   first('.catalog li').click_on 'Add to Cart'
+  #
+  #   assert
+
+  end
