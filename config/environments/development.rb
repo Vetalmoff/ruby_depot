7f13b9a0db30d776e814a2e0c6f3b26c5fc59a23
Vitalii Tymofiichuk <vetalmoff@gmail.com>
2020-03-07 11:44:03 +0200

    Email I1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,9 +31,25 @@ config.webpacker.check_yarn_integrity = true
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
+
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.delivery_method = :smtp
+
+config.action_mailer.smtp_settings = {
+    address:              "smtp.gmail.com",
+    port:                  587,
+    domain:                 "gmail.com",
+    user_name:              "vetalmoff@gmail.com",
+    password:               "yrlbvugreatxxcbc",
+
+    authentication:         :plain,
+    enable_startls_auto:      true
+}
+
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
