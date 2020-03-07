6b1a0cc05698fcc97b7fac02335bbedad0d176bf
Vitalii Tymofiichuk <vetalmoff@gmail.com>
2020-03-07 20:05:34 +0200

    System Test for Email

--- test/system/orders_test.rb
+++ test/system/orders_test.rb
@@ -1,7 +1,13 @@
 require "application_system_test_case"
 
 class OrdersTest < ApplicationSystemTestCase
+  include ActiveJob::TestHelper
+
   test "check routing number" do
+
+    LineItem.delete_all
+    Order.delete_all
+
     visit store_index_url
 
     first('.catalog li').click_on 'Add to Cart'
@@ -16,15 +22,38 @@ class OrdersTest < ApplicationSystemTestCase
 
     select 'Check', from: 'pay_type'
     assert_selector "#order_routing_number"
-    assert_selector "#order_account_number"
 
-    select 'Credit card', from: 'pay_type'
-    assert_selector '#order_credit_card_number'
-    assert_selector '#order_experation_date'
+    fill_in "Routing #", with: "123456"
+    fill_in "Account #", with: "987654"
 
-    select 'Purchase order', from: 'pay_type'
-    assert_selector '#order_po_number'
+    perform_enqueued_jobs do
+      click_button "Place Order"
+    end
 
+    orders = Order.all
+    assert_equal 1, orders.size
+
+    order = orders.first
+
+    assert_equal "Dave Thomas",       order.name
+    assert_equal "123 Main Street",   order.address
+    assert_equal "dave@example.com", order.email
+    assert_equal "Check",            order.pay_type
+    assert_equal 1, order.line_items.size
+
+    mail = ActionMailer::Base.deliveries.last
+    assert_equal ["dave@example.com"],                 mail.to
+    assert_equal 'ruby_depot <vetalmoff@gmail.com>',       mail[:from].value
+    assert_equal "Pragmatic Store Order Confirmation", mail.subject
+
+
+#     "{select 'Credit card', from: 'pay_type'
+#     assert_selector '#order_credit_card_number'
+#     assert_selector '#order_experation_date'
+#
+#     select 'Purchase order', from: 'pay_type'
+#     assert_selector '#order_po_number'
+# }
   end
 
   # test "Add to Cart" do
